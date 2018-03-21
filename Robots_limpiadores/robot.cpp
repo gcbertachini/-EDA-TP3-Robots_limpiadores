34b376d4dba382a20ef15fa39654411b5091b065
Rubidio24 <31071918+Rubidio24@users.noreply.github.com>
2018-03-20 21:46:57 -0300

    Update robot.cpp

--- Robots_limpiadores/robot.cpp
+++ Robots_limpiadores/robot.cpp
@@ -3,27 +3,27 @@
 #include "robot.h"
 #include <time.h>
 #include <iostream>
-
+#include "general.h"
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
 #define RADIAN(a) ( (a)/( 180.0/(double)M_PI) )
-
+#define PNGSIZE 100
 
 using namespace std;
 
 void Robot::create(uint maxFils, uint maxCols)
 {
-	srand(time(NULL));
-	location.x = (rand() % maxCols);  //Obtengo una posici髇 en x aleatoria dentro del tablero
-	location.y = (rand() % maxFils); //Obtengo una posici髇 en y aleatoria dentro del tablero
-	dir = (RADIAN(rand() % 360));  //Obtengo una direcci髇 aleatoria entre 0 y 2Pi para luego pasarla a radianes
-	ALLEGRO_BITMAP * imagen = NULL;
+
+	location.x = (rand() % (SCREENSIZE-PNGSIZE));  //Obtengo una posici贸n en x aleatoria dentro del tablero
+	location.y = (rand() % (SCREENSIZE-PNGSIZE)); //Obtengo una posici贸n en y aleatoria dentro del tablero
+	dir = (RADIAN(rand() % 360));  //Obtengo una direcci贸n aleatoria entre 0 y 2Pi para luego pasarla a radianes
+	
 }
 
 position_t Robot::getPos()
 {
-	return location;  //Devuelvo el dato miembro de la locaci髇 del robot
+	return location;  //Devuelvo el dato miembro de la locaci贸n del robot
 }
 
 void Robot::update(uint maxFils, uint maxCols)
@@ -31,10 +31,10 @@ void Robot::update(uint maxFils, uint maxCols)
 	bool can_move = true;
 	position_t next_location;
 
-	next_location.x = location.x + cos(dir);  //Calculo la futura posici髇 en x
-	next_location.y = location.y + sin(dir);  //Calculo la futura posici髇 en y
+	next_location.x = location.x + cos(dir);  //Calculo la futura posici贸n en x
+	next_location.y = location.y + sin(dir);  //Calculo la futura posici贸n en y
 
-	if ((maxCols >= floor(next_location.x)) && (maxFils >= floor(next_location.y))) //Me fijo si no se va de la cocina
+	if (!((maxCols-PNGSIZE >= next_location.x) && (maxFils-PNGSIZE >= next_location.y) && (0 <= next_location.y) && (0 <= next_location.x))) //Me fijo si no se va de la cocina
 		can_move = false;
 	else  //Si no se va las posciones futuras pasan a ser las actuales
 	{
@@ -42,13 +42,20 @@ void Robot::update(uint maxFils, uint maxCols)
 		location.y = next_location.y;
 	}
 
-	if (can_move == false) //Si el robot se va del piso, mantengo la posici髇 y consigo una nueva direcci髇 aleatoria
+	if (can_move == false) //Si el robot se va del piso, mantengo la posici贸n y consigo una nueva direcci贸n aleatoria
 		dir = (RADIAN(rand() % 360));
 
 }
 
-void Robot::allegro_robot(const char * nombre_archivo)	//AGREGADO POR RUBIDIO, VERIFICAR
+void Robot::allegro_robot(const char * nombre_archivo, double x, double y)	//AGREGADO POR RUBIDIO, VERIFICAR
 {
-	ALLEGRO_BITMAP * imagen = al_load_bitmap("WallE.png");
-	al_draw_scaled_bitmap(imagen, 0, 0, 1000, 1000, 0, 0, 0, 100, 100);
+	ALLEGRO_BITMAP * imagen = al_load_bitmap(nombre_archivo);
+
+	
+
+	if (imagen != NULL)
+	{
+		al_draw_bitmap(imagen,x ,y,0 );
+
+	}
 }
